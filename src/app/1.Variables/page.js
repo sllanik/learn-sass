de8b47e68c6be670bec1f9cf5ab791faad5493ad
Peter <peter.lin@t-2.net>
2023-12-22 15:30:41 +0100

    initial commit

---
+++ src/app/1.Variables/page.js
@@ -0,0 +1,9 @@
+import styles from './page.module.scss'
+
+export default function Variables() {
+    return (
+        <div>
+            <p className={styles.neki}>variables</p>
+        </div>
+    )
+}
