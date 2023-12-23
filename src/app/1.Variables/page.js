4929ed8a129ea7f3f133ab3a1e9005247ef15505
Peter <peter.lin@t-2.net>
2023-12-23 13:51:57 +0100

    1.Variables commit

--- src/app/1.Variables/page.js
+++ src/app/1.Variables/page.js
@@ -2,8 +2,25 @@ import styles from './page.module.scss'
 
 export default function Variables() {
     return (
-        <div>
-            <p className={styles.neki}>variables</p>
+        <div id={styles.body}>
+            <section className={styles.container}>
+                <section className={styles.card}>
+                    <div className={`${styles.img} ${styles.imgOne}`}></div>
+                    <p className={styles.info}>lorem jebihsfdbiofdih dsahuaidsfghwresgfsdrgsre udifh asuhfusadhmlorem jebihsfdbiofdih dssgrsgsdgfgsdfgahuaid udi  asduih pasidh fd se</p>
+                </section>
+                <section className={styles.card}>
+                    <div className={`${styles.img} ${styles.imgTwo}`}></div>
+                    <p className={styles.info}>lorem jlorem jebihsfdbiogdfhsgfsdfgsfgfdsg udifh asu fh asdumlorem jebihsfdbiofdih dssgrsgsdgfgsdfgahuaid udiih pasidh febi se</p>
+                </section>
+                <section className={styles.card}>
+                    <div className={`${styles.img} ${styles.imgThree}`}></div>
+                    <p className={styles.info}>loremlorem jebihsfdbiofdih dssgrsmlorem jebihsfdbiofdih dssgrsgsdgfgsdfgahuahuaid udifh asuh fh asduih pasidh f jebi se</p>
+                </section>
+                <section className={styles.card}>
+                    <div className={`${styles.img} ${styles.imgFour}`}></div>
+                    <p className={styles.info}>loremlorem jebihsfdbiofsfgsersh dsahuaid udifh asu fh asduimlorem jebihsfdbiofdih dssgrsgsdgfgsdfgahuaid udih pasidh f jebi se</p>
+                </section>
+            </section>
         </div>
     )
 }
--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,12 @@
 import Image from 'next/image'
 import styles from './page.module.scss'
+import Link from "next/link";
 
 export default function Home() {
   return (
     <div>
+        <Link href={"/1.Variables"}>1.Variables</Link>
+        <Link href={"/2.Nesting"}>2.Nesting</Link>
       <p>nekineki</p>
     </div>
   )
