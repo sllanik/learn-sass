d8ddc31fd556023a47585254795792d39853081d
Peter <peter.lin@t-2.net>
2023-12-26 17:52:05 +0100

    Main page design

--- src/app/page.js
+++ src/app/page.js
@@ -5,11 +5,55 @@ import Link from "next/link";
 export default function Home() {
   return (
     <div className={styles.openingMenu}>
-        <Link href={"/1.Variables"}>1.Variables</Link>
-        <Link href={"/2.Nesting"}>2.Nesting</Link>
-        <Link href={"/3.Imports"}>3.Imports</Link>
-        <Link href={"/4.Mixins"}>4.Mixins</Link>
-        <Link href={"/5.Extends"}>5.Extends</Link>
+        <div className={styles.Header}>
+            <img src="images/sass.png" alt="sass" className={styles.sass}/>
+            <h1>Slanik Learning Sass in 10 Projects</h1>
+            <p>December 2023 -</p>
+        </div>
+        <div className={styles.cards}>
+            <a href={"/1.Variables"} className={styles.link}>
+            <button className={`${styles.card} ${styles.cardOne}`}>
+                <h2>01</h2>
+                <h3>Variables</h3>
+                <p>23.12.2023</p>
+            </button>
+            </a>
+            <a href={"/2.Nesting"} className={styles.link}>
+                <button className={`${styles.card} ${styles.cardTwo}`}>
+                    <h2>02</h2>
+                    <h3>Nesting</h3>
+                    <p>23.12.2023</p>
+                </button>
+            </a>
+            <a href={"/3.Imports"} className={styles.link}>
+                <button className={`${styles.card} ${styles.cardThree}`}>
+                <h2>03</h2>
+                    <h3>Imports</h3>
+                    <p>23.12.2023</p>
+                </button>
+            </a>
+            <a href={"/4.Mixins"} className={styles.link}>
+                <button className={`${styles.card} ${styles.cardFour}`}>
+                    <h2>04</h2>
+                    <h3>Mixins</h3>
+                    <p>23.12.2023</p>
+                </button>
+            </a>
+            <a href={"/5.Extends"} className={styles.link}>
+                <button className={`${styles.card} ${styles.cardFive}`}>
+                    <h2>05</h2>
+                    <h3>Extends</h3>
+                    <p>23.12.2023</p>
+                </button>
+            </a>
+            <a href={"/6.Operators"} className={styles.link}>
+                <button className={`${styles.card} ${styles.cardSix}`}>
+                    <h2>06</h2>
+                    <h3>Operators</h3>
+                    <p>23.12.2023</p>
+                </button>
+            </a>
+        </div>
     </div>
   )
 }
